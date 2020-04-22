22e576aa98ae5d2e056c34d1f81de91bf4f56b21
Zoltan Jakubecz <jakuzo@gmail.com>
2020-04-22 11:27:34 +0200

    add Modal

---
+++ src/components/Modal.js
@@ -0,0 +1,73 @@
+import React, { useState, forwardRef, useImperativeHandle } from 'react';
+import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+
+const ModalWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+`
+
+const ModalBackDrop = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 100;
+  background-color: rgba(0, 0, 0, 0.8);
+`
+
+const ModalBox = styled.div`
+  text-align: center;
+  font-size: 1.8em;
+  position: relative;
+  padding: 5%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  height: 80%;
+  width: 45%;
+  background-color: rgba(198,198,198,0.9);
+  box-shadow: 0 0 10px rgba(0,0,0,0.25);
+  border-radius: 20px;
+  z-index: 101;
+  overflow-y: auto;
+`
+
+const Modal = forwardRef((props, ref) => {
+
+  const [display, setDisplay] = useState(false);
+
+  useImperativeHandle(ref, () => {
+    return {
+      openModal: () => open(),
+      closeModal: () => close()
+    }
+  });
+
+  const open = () => {
+    setDisplay(true);
+  }
+
+  const close = () => {
+    setDisplay(false);
+  }
+
+  if (display) {
+    return ReactDOM.createPortal(
+      <ModalWrapper>
+        <ModalBackDrop />
+        <ModalBox>
+          {props.children}
+        </ModalBox>
+      </ModalWrapper>, document.getElementById("modal-root")
+    )
+
+  }
+  return null;
+});
+
+export default Modal;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Card } from 'antd';
+import { Card, Button } from 'antd';
+import Modal from './Modal';
+
 
 export default function Movie(props) {
 
@@ -27,29 +29,41 @@ export default function Movie(props) {
     fontweight: '900',
   };
   
+  const modalRef = React.useRef();
+  const openModal = () => {
+    modalRef.current.openModal()
+  }
+
   return (
     <div>
       <div className="site-card-border-less-wrapper">
         <Card className="card">
-          <Card.Grid hoverable={false} style={titleStyle}><strong>{props.movie.title}</strong></Card.Grid>
+          <Card.Grid hoverable={true} style={titleStyle}><strong>{props.movie.title}</strong></Card.Grid>
           <Card.Grid hoverable={false} style={titleStyle1}>
             <div>
               <img src={props.movie.imageURL} alt='Kukutyin' height='300' />
             </div>
             <div>
-            <Card.Grid style={gridStyle}>Category: {props.movie.categories.join(", ")}</Card.Grid>
-            <Card.Grid style={gridStyle}>Director: {props.movie.director}</Card.Grid>
-            <Card.Grid style={gridStyle}>{props.movie.releaseDate}</Card.Grid>
-            </div>
-            
-          </Card.Grid>
-          
-          
-          
-          
+            <Card.Grid hoverable={false} style={gridStyle}>Category: {props.movie.categories.join(", ")}</Card.Grid>
+            <Card.Grid hoverable={false} style={gridStyle}>Director: {props.movie.director}</Card.Grid>
+            <Card.Grid hoverable={false} style={gridStyle}>{props.movie.releaseDate}</Card.Grid>
+            <Card.Grid hoverable={false} style={gridStyle}><Button onClick={openModal}>Edit</Button></Card.Grid>
+            </div>            
+          </Card.Grid>          
         </Card>
       </div>
-            {/* <h1>{props.movie.title}</h1> */}
+
+      <Modal ref={modalRef}>
+        <h1><strong>{props.movie.title}</strong></h1>
+        <form>
+        <label for="title">Movie title: </label>
+          <input type="text" id="title" placeholder={props.movie.title} />
+          <button type="submit">Submit</button>
+        </form>
+        
+        
+        <Button onClick={() => modalRef.current.closeModal()}>Close</Button>
+      </Modal>     {/* <h1>{props.movie.title}</h1> */}
     </div>
     )
 }
