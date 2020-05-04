d1118702fac28a28ee3d74d4ff49faf68a416441
Zoltan Jakubecz <jakuzo@gmail.com>
2020-05-04 11:30:21 +0200

    larger modal window

--- src/components/movie-list/movie-card/Modal.js
+++ src/components/movie-list/movie-card/Modal.js
@@ -29,7 +29,7 @@ const ModalBox = styled.div`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  height: 60%;
+  height: 70%;
   width: 45%;
   background-color: white;
   box-shadow: 0 0 10px rgba(0,0,0,0.25);
