4c1c1a9b33bc60b6ba6c82fb8c31a670a4140569
botond <belgianmaniax@gmail.com>
2020-05-20 10:46:59 +0200

    Configure api/movies requests to accept CORS

--- src/contexts/MovieContext.js
+++ src/contexts/MovieContext.js
@@ -3,6 +3,11 @@ import axios from 'axios';
 
 export const MovieContext = createContext();
 
+const requestConfig = {
+  withCredentials: true, headers: {
+    "Access-Control-Allow-Credentials": "true"
+  },
+}
 
 export const MovieProvider = props => {
   const [data, setData] = useState({
@@ -13,19 +18,19 @@ export const MovieProvider = props => {
 
   useEffect(() => {
     async function getData() {
-      const res = await axios.get("http://localhost:8080/api/movies/?page=1");
+      const res = await axios.get("http://localhost:8080/api/movies/?page=1", requestConfig);
       setData(Object.assign(res.data, { page: 1 }));
     }
     getData();
   }, []);
 
   const loadPage = async function (page) {
-    const res = await axios.get("http://localhost:8080/api/movies/?page=" + page);
+    const res = await axios.get("http://localhost:8080/api/movies/?page=" + page, requestConfig);
     setData(Object.assign(res.data, { page }));
   }
 
   const add = async function (movie) {
-    const res = await axios.post("http://localhost:8080/api/movies", movie);
+    const res = await axios.post("http://localhost:8080/api/movies", movie, requestConfig);
     setData(Object.assign({ ...data }, {
       totalMovieCount: data.totalMovieCount + 1,
       movies: [res.data, ...(data.movies.slice(0, 4))]
@@ -33,23 +38,23 @@ export const MovieProvider = props => {
   }
 
   const update = async function (id, movie) {
-    await axios.put("http://localhost:8080/api/movies/" + id, movie);
+    await axios.put("http://localhost:8080/api/movies/" + id, movie, requestConfig);
   }
 
   const attachCategory = async function (movieId, category) {
-    return await axios.post(`http://localhost:8080/api/movies/${movieId}/categories`, category, {
+    return await axios.post(`http://localhost:8080/api/movies/${movieId}/categories`, category, Object.assign({
       headers: {
         'Content-Type': 'text/plain'
       }
-    });
+    }, requestConfig));
   }
 
   const detachCategory = function (movieId, categoryId) {
-    axios.delete(`http://localhost:8080/api/movies/${movieId}/categories/${categoryId}`);
+    axios.delete(`http://localhost:8080/api/movies/${movieId}/categories/${categoryId}`, requestConfig);
   }
 
   async function handleDeleteFromContext(id) {
-    await axios.delete("http://localhost:8080/api/movies/" + id);
+    await axios.delete("http://localhost:8080/api/movies/" + id, requestConfig);
     await loadPage(data.page);
   }
 
